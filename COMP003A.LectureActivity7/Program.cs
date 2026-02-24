0cc8de7be6d32a543f92d19c1d8b3fdabbf36923
samian <sam.iannazzo@yahoo.com>
2026-02-23 20:13:45 -0800

    added for loop for the array

--- COMP003A.LectureActivity7/Program.cs
+++ COMP003A.LectureActivity7/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             int[] score = { 85, 90, 78, 92, 88 };
+            for (int i = 0; i < score.Length; i++)
+            {
+                Console.WriteLine(score[i]);
+            }
         }
     }
 }
